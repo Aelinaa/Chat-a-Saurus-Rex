980bd99ee68f8b23c5999c4803256e919461070d
Aelinaa <159040620+Aelinaa@users.noreply.github.com>
2025-03-09 12:55:34 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chat_a_Saurus_Rex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
